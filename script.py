b9aa083ea1abe1338d9d87bd670ebd4ab491965a
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-02-13 18:47:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ import json
 import os
 from os import path
 
+CURRENT_REPO_DIR = os.getcwd()
+warnings_json = path.join(CURRENT_REPO_DIR, "warnings.json")
+
 if not path.exists(warnings_json):
     print(f"Error: {warnings_json} not found")
     exit(1)
